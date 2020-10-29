1f5b775ef199c0078497219dfa49df450dc3de94
kailaix <kailaix@hotmail.com>
2020-10-29 01:42:59 -0700

    add make.sh

--- docs/make.sh
+++ docs/make.sh
@@ -1,1 +1,12 @@
-sh src/assets/Codes/Sin/run.sh 
+cd src/assets/Codes/Sin/
+sh run.sh 
+cd "$(dirname "$0")"
+
+cd src/assets/Poisson/
+sh run.sh 
+cd "$(dirname "$0")"
+
+git add -f src/assets/Codes/Sin/sinloss*.png
+git add -f src/assets/Codes/Poisson/*.png
+git commit -m "collect results"
+git push
